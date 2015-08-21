ac427616563be1d731ac6cc0a576c7620a82e50d
nicolem <nicole.maneth@gmail.com>
2015-08-21 15:06:11 -0500

    email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'cryptic-spire-8992.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => 'smtp.gmail.com',
-      :port                 => 587,
-      :domain               => 'gmail.com',
-      :user_name            => ENV["GMAIL_USERNAME"],
-      :password             => ENV["GMAIL_PASSWORD"],
-      :authentication       => 'login',
-      :enable_starttls_auto => true
+   ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 end
