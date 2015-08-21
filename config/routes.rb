c94780cf592bdcc643d4434e86c2dba9b50fba52
nicolem <nicole.maneth@gmail.com>
2015-08-21 14:36:55 -0500

    added invitable

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :histories
   resources :messages
   resources :users
-  devise_for :admins
   root 'users#index'
+  
+  devise_for :admins, :controllers => { :registrations => "registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
