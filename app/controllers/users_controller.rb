de53ed04d742d4af42bd472b296568226bc34e0e
nicolem <nicole.maneth@gmail.com>
2015-09-01 15:14:20 -0500

    spanish messages added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,7 +75,7 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
     params.require(:user).permit(
-      :id, :phone, :active, :next_message
+      :id, :phone, :active, :language, :next_message
     )
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821150004) do
+ActiveRecord::Schema.define(version: 20150901183803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,12 +59,19 @@ ActiveRecord::Schema.define(version: 20150821150004) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "spanish_messages", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "phone"
     t.boolean  "active"
     t.integer  "next_message"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "language"
   end
 
 end
